2e9c6f3ef6e0fa05ae778371a627da345273733f
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2021-05-21 20:55:40 +0200

    Add a launcher

---
+++ XwaToolsLauncher/XwaToolsLauncher/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XwaToolsLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string ToolsDirectory = @".";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Tools = Tool.GetToolsList(ToolsDirectory);
+
+            this.DataContext = this;
+        }
+
+        public List<Tool> Tools { get; set; }
+
+        private void ToolButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var tool = button.DataContext as Tool;
+
+            try
+            {
+                tool.Launch();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ XwaToolsLauncher/XwaToolsLauncher/Tool.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XwaToolsLauncher
+{
+    public class Tool
+    {
+        public Tool(string exe)
+        {
+            string path = Path.GetFullPath(exe);
+
+            Name = Path.GetFileNameWithoutExtension(path);
+            Category = Path.GetFileName(Path.GetDirectoryName(path));
+            ExePath = path;
+            VersionInfo = FileVersionInfo.GetVersionInfo(path);
+        }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string ExePath { get; set; }
+
+        public FileVersionInfo VersionInfo { get; set; }
+
+        public static List<Tool> GetToolsList(string root)
+        {
+            var tools = new List<Tool>();
+
+            foreach (string directory in Directory.EnumerateDirectories(root))
+            {
+                foreach (string exe in Directory.EnumerateFiles(directory, "*.exe"))
+                {
+                    var tool = new Tool(exe);
+                    tools.Add(tool);
+                }
+            }
+
+            return tools;
+        }
+
+        public void Launch()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = ExePath,
+                WorkingDirectory = Path.GetDirectoryName(ExePath)
+            };
+
+            Process.Start(startInfo);
+        }
+    }
+}
